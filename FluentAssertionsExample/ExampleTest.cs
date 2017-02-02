e4d15231c51d7e79f30b4cba6be967bf11cb7e0e
Josh Strohminger <jdst8-git@yahoo.com>
2017-02-01 23:05:11 -0800

    Add pass and fail examples.

---
+++ FluentAssertionsExample/ExampleTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using FluentAssertions.Common;
+using Xunit;
+
+namespace FluentAssertionsExample
+{
+    public class ExampleTest
+    {
+        [Fact]
+        public void ClassWithoutCctor_Works()
+        {
+            typeof(ClassWithoutCctor).Should().HaveDefaultConstructor();
+        }
+
+        /// <summary>
+        /// This fails because the class has two parameterless constructors and the call to
+        /// HaveDefaultConstructor calls HaveConstructor which calls GetConstructor which
+        /// calls type.GetConstructors(AllMembersFlag).SingleOrDefault which throws an
+        /// exception when more than one element is found.
+        /// </summary>
+        [Fact]
+        public void ClassWithCctor_Fails()
+        {
+            typeof(ClassWithCctor).Should().HaveDefaultConstructor();
+        }
+    }
+
+    /// <summary>
+    /// This class does not have a cctor, just a parameterless ctor, the default constructor.
+    /// </summary>
+    internal class ClassWithoutCctor
+    {
+        private static int Value { get; set; }
+    }
+
+    /// <summary>
+    /// This class has both a parameterless cctor, because the static property is initialized,
+    /// and a parameterless ctor, the default constructor.
+    /// </summary>
+    internal class ClassWithCctor
+    {
+        private static int Value { get; set; } = 0;
+    }
+}
